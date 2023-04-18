b82c2a772da99c76cc832b75081677baf05dad57
gdstzmy <gdstzmy1998@outlook.com>
2023-04-18 23:39:32 +0800

    NJU-ProjectN/nanos-lite ics2021 initialized
    NJU-ProjectN/nanos-lite 2a141760e31be246a7316942293a97873925bc2f Makefile: use header files in newlib-navy

--- ysyx-workbench/nemu/src/cpu/difftest/ref.c
+++ ysyx-workbench/nemu/src/cpu/difftest/ref.c
@@ -19,15 +19,41 @@
 #include <memory/paddr.h>
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+    if (direction == DIFFTEST_TO_DUT)
+  {
+    buf = (void *)guest_to_host(addr);
+  }
+  else if (direction == DIFFTEST_TO_REF)
+  {
+    Log("%x,%lx", addr, n);
+    char *buf_char = (char *)buf;
+    for (int i = 0; i < n; i++)
+    {
+      paddr_write(addr + i, 1, buf_char[i]);
+    }
+  }
 }
 
-void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+void difftest_regcpy(void *dut, bool direction)
+{
+  if (direction == DIFFTEST_TO_DUT)
+  {
+    for (int i = 0; i < 32; i++)
+      ((CPU_state *)dut)->gpr[i] = cpu.gpr[i];
+    ((CPU_state *)dut)->pc = cpu.pc;
+  }
+  else if (direction == DIFFTEST_TO_REF)
+  {
+    for (int i = 0; i < 32; i++)
+      cpu.gpr[i] = ((CPU_state *)dut)->gpr[i];
+    cpu.pc = ((CPU_state *)dut)->pc;
+    // csr don't sync
+    // cpu = *(CPU_state *)dut;
+  }
 }
 
 void difftest_exec(uint64_t n) {
-  assert(0);
+  cpu_exec(n);
 }
 
 void difftest_raise_intr(word_t NO) {
@@ -36,5 +62,10 @@ void difftest_raise_intr(word_t NO) {
 
 void difftest_init(int port) {
   /* Perform ISA dependent initialization. */
+  void init_rand();
+  void init_mem();
+  void init_isa();
+  init_rand();
+  init_mem();
   init_isa();
 }
